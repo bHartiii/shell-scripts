963a843bd7ee2f243705f46c9cacfecb2718da91
bharti mali <malibharti5@gmail.com>
2020-10-31 01:54:19 +0530

    MODIFY : Final wage calculated using switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 FULL_TIME=8
-PART_TIME=8
+PART_TIME=4
 wage_per_hour=20
 dailyWage=0
+getWorkDonePerDay(){
+
+        case $((RANDOM%2)) in
+                0) workDonePerDay=$PART_TIME;;
+                1) workDonePerDay=$FULL_TIME;;
+                *) worDonePerDay=0;;
+        esac
+        echo $workDonePerDay;
+}
+workDonePerDay=$( getWorkDonePerDay $((RANDOM%2)) );
 getDailyWage(){
-	dailyWage=$((FULL_TIME*wage_per_hour))
+	dailyWage=$((workDonePerDay*wage_per_hour))
 	echo "Employee's daily wage is - "$dailyWage
 }
 
