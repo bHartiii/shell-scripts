7d28e2d2658fa222c405f310b9d11f964e018c1b
bharti mali <malibharti5@gmail.com>
2020-10-31 01:40:23 +0530

    ADD : Employee daily wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -x
+FULL_TIME=8
+PART_TIME=4
+wage_per_hour=20
+dailyWage=0
+getDailyWage(){
+	dailyWage=$((FULL_TIME*wage_per_hour))
+	echo "Employee's daily wage is - "$dailyWage
+}
+
 attendance=$((RANDOM%2))
 if [ $attendance -eq 0 ]
 then
 	echo "Employee is absent."
 else 
-	echo "Employee is present."
+	getDailyWage
 fi
